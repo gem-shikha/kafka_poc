f1e9623b7c21bfaa9caaa22b3899ba46ee16a158
shikha-rawat <105268578+shikha-rawat@users.noreply.github.com>
2022-08-11 11:43:01 +0530

    Update User.java

--- src/main/java/com/springboot/kafka/model/User.java
+++ src/main/java/com/springboot/kafka/model/User.java
@@ -1,14 +1,12 @@
 package com.springboot.kafka.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class User {
 
     private String name;
